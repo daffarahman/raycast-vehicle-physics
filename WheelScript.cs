6038e6bdc0f60d4a388cdc5ce9ee7e47cf0fb9cf
Daffa Rahman <69158099+daffarahman@users.noreply.github.com>
2021-06-15 07:58:01 +0700

    Update WheelScript.cs

--- WheelScript.cs
+++ WheelScript.cs
@@ -1,1 +1,75 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 
+public class WheelScript : MonoBehaviour {
+
+    private Rigidbody rb;
+
+    public bool wheelFrontLeft;
+    public bool wheelFrontRight;
+    public bool wheelRearLeft;
+    public bool wheelRearRight;
+
+    [Header("Suspension")]
+    public float restLength;
+    public float springTravel;
+    public float springStiffness;
+    public float damperStiffness;
+
+    private float minLength;
+    private float maxLength;
+    private float lastLength;
+    private float springLength;
+    private float springVelocity;
+    private float springForce;
+    private float damperForce;
+
+    [Header("Wheel")]
+    public float steerAngle;
+    public float steerTime;
+
+    private Vector3 suspensionForce;
+    private Vector3 wheelVelocityLS; // Local Space
+    private float Fx;
+    private float Fy;
+    private float wheelAngle;
+    public Vector3 currentVelocity;
+
+    [Header("Wheel")]
+    public float wheelRadius;
+
+    void Start() {
+        rb = transform.root.GetComponent<Rigidbody>();
+
+        minLength = restLength - springTravel;
+        maxLength = restLength + springTravel;
+    }
+
+    void Update() {
+        wheelAngle = Mathf.Lerp(wheelAngle, steerAngle, steerTime * Time.deltaTime);
+        transform.localRotation = Quaternion.Euler(Vector3.up * wheelAngle);
+
+        Debug.DrawRay(transform.position, -transform.up * (springLength + wheelRadius), Color.green);
+    }
+
+    void FixedUpdate() {
+        if(Physics.Raycast(transform.position, -transform.up, out RaycastHit hit, maxLength + wheelRadius)) {
+            lastLength = springLength;
+            springLength = hit.distance - wheelRadius;
+            springLength = Mathf.Clamp(springLength, minLength, maxLength);
+            springVelocity = (lastLength - springLength) / Time.fixedDeltaTime;
+            springForce = springStiffness * (restLength - springLength);
+            damperForce = (damperStiffness * springVelocity);
+
+            suspensionForce = (springForce + damperForce) * transform.up;
+
+            wheelVelocityLS = transform.InverseTransformDirection(rb.GetPointVelocity(hit.point));
+            Fx = Input.GetAxis("Vertical") * 0.4f * springForce;
+            Fy = wheelVelocityLS.x * springForce;
+
+            currentVelocity = suspensionForce + (Fx * transform.forward) + (Fy * -transform.right);
+            rb.AddForceAtPosition(currentVelocity, hit.point);
+        }
+    }
+}
